9c1123a59035674c00cdc5a6041635c61226ac34
Christian B <desarrollowebestudio@gmail.com>
2020-09-02 23:27:27 -0500

    235 Smooth Scroll

--- JavaScript/aplicacion.js
+++ JavaScript/aplicacion.js
@@ -4,10 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
   // Variables Elementos
   const botonMenu = document.getElementById('botonMenuDesplegable');
   const menuEnlaces = document.getElementById('menuEnlacesNavegacion');
+  const enlacesScroll = document.querySelectorAll("a.enlaceScroll");
+  const barraNavegacion = document.getElementById('encabezadoNavegacion');
 
-  // Comportamiento Menú Enlaces
+  // Comportamiento Desplegable: Menú Enlaces
   botonMenu.addEventListener('click', () => {
     menuEnlaces.classList.toggle('mostrarMenu');
+  });
+
+  // Scroll Secciones
+  enlacesScroll.forEach(enlace => {
+    enlace.addEventListener("click", evento => {
+
+      // Interceptar y evitar navegación por defecto
+      evento.preventDefault();
+
+      // Ocultar menú desplegable
+      menuEnlaces.classList.remove('mostrarMenu');
+
+      // Obtener identificador de elemento destino
+      const idDestino = evento.target.getAttribute('href').slice(1);
+      const elementoDestino = document.getElementById(idDestino);
+
+      // Obtener posición final considerando alto de barra de navegación
+      const posicion = elementoDestino.offsetTop - barraNavegacion.offsetHeight;
+
+      // Realizar scroll suave según posición calculada
+      window.scrollTo({left: 0, top: posicion, behavior: 'smooth'});
+    });
   });
 
 });
